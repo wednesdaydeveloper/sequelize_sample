967a125c776a56eb5804f48a0da948d6ec9edd31
tokamoto1971 <tokamoto6676+github2@gmail.com>
2016-02-21 13:26:59 +0900

    M_AREA のデータ型を宣言

--- selectFromMSSQL.js
+++ selectFromMSSQL.js
@@ -15,7 +15,45 @@ const sequelize = new Sequelize('Claire3_19', 'rm', 's3cret', {
 			}
 		});
 
-sequelize.query("SELECT * FROM [M_AREA]", { type: sequelize.QueryTypes.SELECT})
-  .then(function(area) {
-    console.log(JSON.stringify(area))
-  })
+const Area = sequelize.define('M_AREA', {
+  name: {
+    type: Sequelize.STRING,
+    field: 'NAME'
+  },
+	displayOrder: {
+    type: Sequelize.INTEGER,
+		field: 'DISPLAY_ORDER'
+  },
+	updateDate: {
+    type: Sequelize.TIME,
+		field: 'UPDATE_DATE'
+  },
+	updateUserId: {
+    type: Sequelize.INTEGER,
+		field: 'UPDATE_USER_ID'
+  },
+	isDeleted: {
+    type: Sequelize.BOOLEAN,
+		field: 'IS_DELETED'
+  }
+}, {
+	timestamps: false,
+  freezeTableName: true // Model tableName will be the same as the model name
+});
+
+// Areaを取得
+Area.findAll()
+.then(areas => {
+	areas.forEach(function (area) {
+		console.log(JSON.stringify(area));
+	});
+})
+.catch(err => {
+    if(err)
+      console.error(JSON.stringify(err));
+});
+
+// sequelize.query("SELECT * FROM [M_AREA]", { type: sequelize.QueryTypes.SELECT})
+//   .then(function(area) {
+//     console.log(JSON.stringify(area))
+//   })
